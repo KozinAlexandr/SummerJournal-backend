7f711f505a6c78e80d5ac130f7d70ef499c5ba3b
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-04-06 02:48:17 +0300

    Post search (problem with ILIKE and LIKE), recent and popular posts, QueryBuilder

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -4,6 +4,7 @@ import { UpdatePostDto } from './dto/update-post.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PostEntity } from './entities/post.entity';
+import { SearchPostDto } from './dto/search-post.dto';
 
 @Injectable()
 export class PostService {
@@ -17,17 +18,70 @@ export class PostService {
   }
 
   findAll() {
-    return this.repository.find();
+    return this.repository.find({
+      //возвращаем по последнему времени создания посты
+      order: {
+        createdAt: 'DESC',
+      },
+    });
+  }
+
+  //find объеденяет все в массив записей, но мне это не нужно
+  //я хочу запрашивать что-то конкретное, поэтому use queryBuilder
+  async popular() {
+    const qb = this.repository.createQueryBuilder();
+
+    qb.orderBy('views', 'DESC');
+    qb.limit(10);
+
+    const [items, total] = await qb.getManyAndCount();
+
+    return { items, total };
+  }
+
+  async search(dto: SearchPostDto) {
+    const qb = this.repository.createQueryBuilder('p');
+
+    qb.limit(dto.limit || 0);
+    qb.take(dto.take || 10);
+    if (dto.views) {
+      qb.orderBy('views', dto.views);
+    }
+    if (dto.body) {
+      qb.andWhere('p.body LIKE :body'); //maybe ILIKE postgre  - LIKE mysql
+    }
+
+    if (dto.title) {
+      qb.andWhere('p.title LIKE :title');
+    }
+    if (dto.tag) {
+      qb.andWhere('p.tags LIKE :tag');
+    }
+    //console.log(qb.getSql());
+    qb.setParameters({
+      title: '%${dto.title}%',
+      body: '%${dto.body}%',
+      tag: '%${dto.tag}%',
+      views: dto.views || '',
+    });
+
+    const [items, total] = await qb.getManyAndCount();
+
+    return { items, total };
   }
 
   async findOne(id: number) {
-    //Проверка есть статья или нету
-    const find = await this.repository.findOne(+id);
-    if (!find) {
-      throw new NotFoundException('Статья не найдена');
-    }
-    //возвращаем то что есть
-    return find;
+    //Инкрементируем views
+    await this.repository
+      .createQueryBuilder('posts')
+      .whereInIds(id)
+      .update()
+      .set({
+        views: () => 'views + 1',
+      })
+      .execute();
+
+    return this.repository.findOne(id);
   }
 
   async update(id: number, dto: UpdatePostDto) {
